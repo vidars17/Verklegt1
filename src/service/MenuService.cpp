05b01fcf4a9e0ce9ebcba4e490b731d800ec68af
Bjossidan <34061953+Bjossidan@users.noreply.github.com>
2017-12-06 23:06:50 +0000

    Small changes
    Added some couts to make it more stream lined
    
    Found a bug when entering toppings and the price
    Suggestion: changing the price from int to char

--- src/service/MenuService.cpp
+++ src/service/MenuService.cpp
@@ -6,8 +6,11 @@ MenuService::MenuService()
 }
 
 void MenuService::addPizza() {
+    cout << "Please enter Name of Pizza" << endl;
     cin >> name;
+    cout << "Please Enter desired toppings" << endl;
     cin >> topping;
+    cout << "What is the price of this bad boy"  << endl;
     cin >> price;
 
     Pizza pizza(name, topping, price);
